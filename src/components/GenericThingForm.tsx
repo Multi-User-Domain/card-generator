5bc78af3b159b9cd46bb5f1f5d025f8360df251f
Calum Mackervoy <c.mackervoy@gmail.com>
2023-07-29 22:17:48 +0200

    feature (GenericThingForm): loads shape from given URI

--- src/components/GenericThingForm.tsx
+++ src/components/GenericThingForm.tsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import validator from 'validator';
+import axios from 'axios';
 
 interface IGenericThingForm {
     id?: string,
@@ -9,6 +11,21 @@ interface IGenericThingForm {
 const GenericThingForm = ({id, shape}: IGenericThingForm) => {
 
     const [generatedId, setGeneratedId] = useState<string|null>(null);
+    const [loadedShape, setLoadedShape] = useState<string|null>(null);
+    const [loadingContent, setLoadingContent] = useState<string>("Loading...");
+
+    // on first rendering, load the shape string from the given URL
+    useEffect(() => {
+        if(validator.isURL(shape)) {
+            axios.get(shape).then((res) => {
+                setLoadedShape(res.data);
+            })
+            .catch((err: any) => {
+                setLoadingContent("Error loading form: " + err);
+            });
+        }
+        else setLoadedShape(shape);
+    }, []);
 
     // auto-generate unique id if none given by user
     if(id == null) {
@@ -20,9 +37,13 @@ const GenericThingForm = ({id, shape}: IGenericThingForm) => {
         }
     }
 
+    if(loadedShape == null) {
+        return <p>{loadingContent}</p>;
+    }
+
     // shacl-form is a HTMLElement. Convert to a React element during rendering
     let form = React.createElement("shacl-form", {
-        "data-shapes": shape,
+        "data-shapes": loadedShape,
         "id": id,
         "className": "genric-thing-form"
     });
